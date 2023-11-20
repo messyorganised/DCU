43047924600d43f44787f086e6193afa5afc2d8a
messyorganised <100069865+messyorganised@users.noreply.github.com>
2023-11-21 10:32:12 +1100

    Added array table rather than individual line coding

--- CheckDCUInstall.ps1
+++ CheckDCUInstall.ps1
@@ -5,7 +5,7 @@ $DCUVersion = "5.0.0"
 $DCUCurrentInstall = Get-WmiObject -class Win32_Product | Where-Object {$_.Name -like "*Dell Command*"}
 
 function DCUUninstall{
-    #Start-Process "msiexec.exe" -ArgumentList "/x", $DCUCurrentInstall.IdentifyingNumber, "/passive", "/quiet", "/norestart" -Wait -NoNewWindow
+    Write-Host "Outdated version detected. Uninstalling said version now..."
     msiexec.exe /x $DCUCurrentInstall.IdentifyingNumber /passive /quiet /norestart | Write-Verbose
     Write-Host "Uninstall completed."
 
@@ -15,23 +15,35 @@ function DCUInstall {
     [System.IO.Directory]::CreateDirectory('C:\Temp')
     Invoke-WebRequest -Uri $DCULink -OutFile C:\Temp\DCU.exe -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
     Start-Process C:\Temp\DCU.exe -Wait -ArgumentList @("/s") 
+    Write-Host "Installation completed successfully."
 }
-function UpdateDrivers {
-    $delllogs = 'C:\dell\logs\'
 
-    Write-Host "Begin Update Process"
-    Start-Process "C:\Program Files (X86)\Dell\CommandUpdate\dcu-cli.exe" -WindowStyle Hidden -Wait -ArgumentList @("/configure","-silent","-autoSuspendBitLocker=enable","-userConsent=disable")
-    Start-Process "C:\Program Files (X86)\Dell\CommandUpdate\dcu-cli.exe" -WindowStyle Hidden -Wait -ArgumentList @("/scan","-silent","-outputLog=C:\dell\logs\scan.log")
-    Write-Host (Get-Content "$delllogs\scan.log"  | Select-String "Number of applicable updates for the current system configuration")
-    Start-Process "C:\Program Files (X86)\Dell\CommandUpdate\dcu-cli.exe" -WindowStyle Hidden -Wait -ArgumentList @("/ApplyUpdates","-silent","-reboot=disable","-outputLog=C:\dell\logs\ApplyUpdates.log")
-    Write-Host (Get-Content "$delllogs\ApplyUpdates.log" | Select-String "Finished installing the updates.")
-    Write-Host (Get-Content "$delllogs\ApplyUpdates.log" | Select-String "The program exited with return code")
+function UpdateDrivers {
+    param (
+        [string]$DCUCLI = "C:\Program Files (X86)\Dell\CommandUpdate\dcu-cli.exe",
+    )
+
+    Writ-Host "Starting Dell Updates..."
+    # Define the commands and their corresponding arguments using the variables
+    $DCUCommands = @{
+        Configure = @("/configure","-silent","-autoSuspendBitLocker=enable","-userConsent=disable")
+        Scan = @("/scan","-silent","-outputLog="C:\dell\logs\scan.log")
+        ApplyUpdates = @("/ApplyUpdates","-silent","-reboot=disable","-outputLog="C:\dell\logs\ApplyUpdates.log")
+    }
+
+    foreach ($command in $DCUCommands.GetEnumerator()) {
+        # Replace placeholders with actual paths
+        Start-Process $DCUCLI -WindowStyle Hidden -Wait -ArgumentList $arguments
+
+        # Additional processing specific to the scan log
+        if ($command.Key -eq 'Scan') {
+            Write-Host (Get-Content ""C:\dell\logs\scan.log" | Select-String "Number of applicable updates for the current system configuration")
+        }
+    }
 }
 
 if ($DCUCurrentInstall.Version -notlike $DCUVersion) {
-        Write-Host "Outdated version detected. Uninstalling said version now..."
         DCUUninstall
-
         DCUInstall
     }
 else {
@@ -39,4 +51,3 @@ else {
     }
 
 UpdateDrivers
-
